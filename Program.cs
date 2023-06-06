ad6cf502c7a5d4d58b18232e455a263a40c3d95a
MarsLuz <123537741+MarsLuz@users.noreply.github.com>
2023-06-06 14:42:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the width of the room: ");
+        int width = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the height of the room: ");
+        int height = int.Parse(Console.ReadLine());
+
+        int totalTiles = width * height;
+
+        if (totalTiles % 4 == 0 || totalTiles % 4 == 0)
+        {
+            Console.WriteLine("Too stuffy!");
+        }
+        else
+        {
+            Console.WriteLine("Okay!");
+        }
+    }
+}
